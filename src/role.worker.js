89fb481e972bbd73aadf5a194fc30b97a5599a33
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-11 20:40:14 -0500

    More context references

--- src/role.worker.js
+++ src/role.worker.js
@@ -59,8 +59,8 @@ var roleWorker = {
 		var targets = this.creep.room.find(FIND_CONSTRUCTION_SITES);
 
 		if(targets.length) {
-			if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-				creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+			if(this.creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+				this.creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
 			}
 		}
 	}
