b65956188cc6d15fb89235dd1739ed1ac9ca2ff9
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-11 20:38:33 -0500

    Consolidated worker roles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 var extensionConstructor = require('spawn.extension.constructor');
 
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleWorker = require('role.worker');
 
 module.exports.loop = function () {
     
@@ -13,35 +11,15 @@ module.exports.loop = function () {
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+    var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
+    console.log('Workers: ' + workers.length);
     
-    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    console.log('Upgraders: ' + upgraders.length);
-
-	var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-	console.log('Builders: ' + builders.length);
-
-    if(harvesters.length < 3) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
+    if(workers.length < 10) {
+        var newName = 'Worker' + Game.time;
+        console.log('Spawning new worker: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});
+            {memory: {role: 'worker'}});
     }
-    
-    else if(upgraders.length < 3) {
-        var newName = 'Upgrader' + Game.time;
-        console.log('Spawning new upgrader: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'upgrader'}});
-    }
-
- 	else if(builders.length < 3) {
- 	         var newName = 'Builder' + Game.time;
- 	         console.log('Spawning new builder: ' + newName);
- 	         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
- 	             {memory: {role: 'builder'}});
- 	}
     
     if(Game.spawns['Spawn1'].spawning) { 
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
@@ -52,22 +30,15 @@ module.exports.loop = function () {
             {align: 'left', opacity: 0.8});
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-		if(creep.memory.role == 'builder') {
-			roleBuilder.run(creep);
-		}
-    }
+    	for(var name in Game.creeps) {
+        	var creep = Game.creeps[name];
+        	if(creep.memory.role == 'worker') {
+            	roleWorker.run(creep);
+        	}
+    	}
 	
-		// Need to fix building, blocking movement and will build infinitely
-		var response = extensionConstructor.run(Game.spawns['Spawn1']);
-		console.log(response); 
+	var response = extensionConstructor.run(Game.spawns['Spawn1']);
+	console.log(response); 
     
     if(Game.cpu.bucket == 10000) {
         Game.cpu.generatePixel();
---
+++ src/role.worker.js
@@ -0,0 +1,69 @@
+const WORKER_HARVESTING = 'harvesting';
+const WORKER_UPGRADING = 'upgrading';
+const WORKER_BUILDING = 'building';
+
+
+var roleWorker = {
+
+	run: function(creep) {
+		this.creep = creep;
+		this.assignTask();
+		this.performTask(creep.memory.task);
+	},
+
+	assignTask: function() {
+		if(creep.store[RESOURCE_ENERGY] === 0) {
+			creep.memory.task = WORKER_HARVESTING;
+		}
+		else {
+			if (creep.room.find(FIND_CONSTRUCTION_SITES).length) {
+				creep.memory.task = WORKER_BUILDING;
+			}
+			else {
+				creep.memory.task = WORKER_UPGRADING;
+			}
+		}
+	},
+
+	performTask: function(task) {
+		switch(task) {
+			case WORKER_HARVESTING:
+				this.harvest();
+				break;
+
+			case WORKER_UPGRADING:
+				this.upgrade();
+				break;
+
+			case WORKER_BUILDING:
+				this.build();
+				break;
+		}
+	},
+
+	harvest: function() {
+		var source = this.creep.pos.findClosestByPath(FIND_SOURCES);
+
+		if(source != null && this.creep.harvest(source, RESOURCE_ENERGY) === ERR_NOT_IN_RAGE) {
+			this.creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+		}
+	},
+
+	upgrade: function() {
+		if(creep.upgradeController(this.creep.room.controller) === ERR_NOT_IN_RANGE) {
+			creep.moveTo(this.creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+		}
+	},
+
+	build: function() {
+		var targets = this.creep.room.find(FIND_CONSTRUCTION_SITES);
+
+		if(targets.length) {
+			if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+				creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+			}
+		}
+	}
+};
+
+module.exports = roleWorker;
