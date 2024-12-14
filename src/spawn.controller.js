c19e9caf54614785b28254f87daa6b7d41b97fde
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-13 20:43:25 -0500

    Fix hash key reference

--- src/spawn.controller.js
+++ src/spawn.controller.js
@@ -4,7 +4,7 @@ var spawnController = {
 		
 	run: function() {
 		var spawnHash = Game.spawns;
-		var spawns = Game.spawns.map(function(v) { return spawnHash[v]; });
+		var spawns = spawnHash.keys.map(function(v) { return spawnHash[v]; });
 		var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
 
 		if (workers.length < MAX_WORKERS){
