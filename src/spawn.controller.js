0708e6817634bb317da84c793c18e7ca8ee3c6df
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-13 20:40:06 -0500

    Basic spawn controller

---
+++ src/spawn.controller.js
@@ -0,0 +1,17 @@
+const MAX_WORKERS = 10;
+
+var spawnController = {
+		
+	run: function() {
+		var spawnHash = Game.spawns;
+		var spawns = Game.spawns.map(function(v) { return spawnHash[v]; });
+		var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
+
+		if (workers.length < MAX_WORKERS){
+			var newName = 'Worker' + Game.time;
+			spawns[0].spawnCreep([WORK, CARRY, MOVE], newName,
+				{memory: {role: 'worker'}});
+	} 
+};
+
+modle.exports = spawnController;
