82bf045f07c743e7241404e95ea1425b53df522c
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-17 14:09:05 -0500

    Fix syntax error

--- src/role.worker.js
+++ src/role.worker.js
@@ -33,17 +33,17 @@ var roleWorker = {
 		var target = this.creep.pos.findClosestByPath(FIND_STRUCTURES, {
 			filter: (structure) => {
 				return (structure.structureType === STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN ||
-					structure.structureType == STRUCTURE_STORAGE) &&
-					structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+					structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0)
 			}
 		});
 		if (target === null){
 			target = this.creep.pos.findClosestByPath(FIND_STRUCTURES, {
 				filter: (structure) => {
 					return (structure.structureType === STRUCTURE_STORAGE &&
-						structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+						structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0)
 				}
-		});
+			});
+		}
 		return target;	
 	},
 
