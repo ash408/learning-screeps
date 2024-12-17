27380085ebcc76e727042af34757f967a865c406
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-17 08:22:26 -0500

    Add logic for spawning guards

--- src/spawn.controller.js
+++ src/spawn.controller.js
@@ -3,20 +3,36 @@ const MAX_WORKERS = 10;
 const MOVE_COST = 50;
 const WORK_COST = 100;
 const CARRY_COST = 50;
+const ATTACK_COST = 80;
+const TOUGH_COST = 10;
 
-const BODY_HASH = {[MOVE]: MOVE_COST, [WORK]: WORK_COST, [CARRY]: CARRY_COST};
+const BODY_HASH = {[MOVE]: MOVE_COST, [WORK]: WORK_COST, [CARRY]: CARRY_COST
+				[ATTACK]: ATTACK_COST, [TOUGH]: TOUGH_COST};
+
 const WORKER_TEMPLATE = [MOVE, MOVE, CARRY, WORK];
+const GUARD_TEMPLATE = [TOUGH, MOVE, ATTACK, MOVE];
 
 var spawnController = {
 		
 	run: function() {
 		var spawnHash = Game.spawns;
 		var spawns = Object.keys(spawnHash).map(function(v) { return spawnHash[v]; });
+		
 		var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
+		var guards = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard');
+		
+		if (spawns[0].room.find(FIND_HOSTILE_CREEPS).length > guards.length) {
+			var newName = 'Guard' + Game.time;
+			var creepBody = this.calculateBody(spawns[0].room, GUARD_TEMPLATE);
 
-		if (workers.length < MAX_WORKERS) {
+			if (creepBody !== null) {
+				spawns[0].spawnCreep(creepBody, newName,
+					{memory: {role: 'guard'}});
+			}
+		}
+		else if (workers.length < MAX_WORKERS) {
 			var newName = 'Worker' + Game.time;
-			var creepBody = this.calculateBody(spawns[0].room);
+			var creepBody = this.calculateBody(spawns[0].room, WORKER_TEMPLATE);
 
 			if (creepBody !== null) {
 				spawns[0].spawnCreep(creepBody, newName,
@@ -25,8 +41,7 @@ var spawnController = {
 		}
 	},
 
-	calculateBody: function(room) {
-		var template = WORKER_TEMPLATE;
+	calculateBody: function(room, template) {
 		var body = template.slice(0);
 		var totalEnergy = room.energyAvailable;
 
