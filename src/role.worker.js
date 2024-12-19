5f74ed669281445c49eca5c00e23e5213fc46565
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-19 09:13:13 -0500

    Fix transfer stuck

--- src/role.worker.js
+++ src/role.worker.js
@@ -16,7 +16,7 @@ var roleWorker = {
 		if(this.creep.store[RESOURCE_ENERGY] === 0) {
 			this.creep.memory.task = WORKER_HARVESTING;
 		}
-		else if(this.creep.store.getFreeCapacity() === 0){
+		else {
 			if (this.getEmptyStore() !== null) {
 				this.creep.memory.task = WORKER_TRANSFERING;
 			}
@@ -137,10 +137,9 @@ var roleWorker = {
 			if(this.creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
 				this.creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
 			}
+			return;
 		}
-		else {
-			this.creep.memory.task = WORKER_HARVESTING;
-		}
+		this.creep.memory.task = WORKER_HARVESTING;
 	},
 
 	upgrade: function() {
