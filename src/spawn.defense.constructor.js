edd41457f3e7126b30217ac2716ec5c3c9cfebba
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-19 07:46:34 -0500

    Fix coordinate calculation

--- src/spawn.defense.constructor.js
+++ src/spawn.defense.constructor.js
@@ -3,7 +3,7 @@ var spawnDefenseConstructor = {
 
 	run: function(spawn) {
 		var x = spawn.pos.x + 1;
-		var y = spawn.pos.x + 1;
+		var y = spawn.pos.y + 1;
 
 		var hasTower = spawn.room.lookForAt(LOOK_STRUCTURES, x, y).length !== 0;
 		if (!hasTower) {
