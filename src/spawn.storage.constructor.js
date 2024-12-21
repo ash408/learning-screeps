a4a96124f8e6345c1c33cf95c1f34875fcb43556
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-21 08:56:36 -0500

    Add second spawn storage

--- src/spawn.storage.constructor.js
+++ src/spawn.storage.constructor.js
@@ -5,6 +5,15 @@ var spawnStorageConstructor = {
 		var x = spawn.pos.x - 1;
 		var y = spawn.pos.y - 1;
 		
+		this.buildContainer(x, y);
+
+		x = spawn.pos.x - 1;
+		y = spawn.pos.y + 1;
+
+		this.buildContainer(x, y);	
+	},
+
+	buildContainer: function(x, y) {
 		var hasStorage = spawn.room.lookForAt(LOOK_STRUCTURES, x, y).length !== 0;
 		if (!hasStorage) {
 			spawn.room.createConstructionSite(x, y, STRUCTURE_CONTAINER);
