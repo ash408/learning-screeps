513caa80704811dfe6b16a7359e7be07f46ff8c4
ash408 <124532093+ash408@users.noreply.github.com>
2024-12-22 11:23:47 -0500

    Creep sudoku

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -16,6 +16,8 @@ var roleClaimer = {
 				console.log('Room ' + Memory.expansionTarget + ' claimed');
 				console.log('Stopping expansion...');
 				Memory.expansion = false;
+
+				creep.suicide(); // :(
 			}
 		}
 	}
