5ad017e981e9d07699f415fe00873f9861d76d30
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-04 22:18:51 -0500

    Clean main

--- test/main.js
+++ test/main.js
@@ -1,19 +1,14 @@
 "use strict";
 
-//let spawnController = require('spawn.controller');
-//let spawnConstructor = require('spawn.constructor');
-//let extensionConstructor = require('spawn.extension.constructor');
-//let spawnStorageConstructor = require('spawn.storage.constructor');
-//let spawnDefenseConstructor = require('spawn.defense.constructor');
 let roadConstructor = require('road.constructor');
 let colonyController = require('colony.controller');
 let expansionController = require('expansion.controller');
 
-//let roleTower = require('role.tower');
 let roleWorker = require('role.worker');
 let roleUpgrader = require('role.upgrader');
 let roleGuard = require('role.guard');
 let roleClaimer = require('role.claimer');
+
 
 module.exports.loop = function () {
 	if (Memory.hasRoads === undefined) {
@@ -23,35 +18,18 @@ module.exports.loop = function () {
 		Memory.expansion = false;
 	}
     
-    for(let name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-        }
-    }
+    	for(let name in Memory.creeps) {
+     	if(!Game.creeps[name]) {
+          	delete Memory.creeps[name];
+        	}
+    	}
     
-    //if(Game.spawns['Spawn1'].spawning) { 
-    //    let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-    //    Game.spawns['Spawn1'].room.visual.text(
-    //        '🛠️' + spawningCreep.memory.role,
-    //        Game.spawns['Spawn1'].pos.x + 1, 
-    //        Game.spawns['Spawn1'].pos.y, 
-    //        {align: 'left', opacity: 0.8});
-   // }
-	
-	//let towers = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
-	//	filter: (t) => {
-	//		return t.structureType == STRUCTURE_TOWER &&
-	//			t.store.getCapacity(RESOURCE_ENERGY) > 0;
-	//	}
-	//});
-	//for (let tower of towers) {
-	//	roleTower.run(tower);
-	//} 
 	colonyController.run();
 
     	for(let name in Game.creeps) {
         	let creep = Game.creeps[name];
-        	if(creep.memory.role == 'worker') {
+        	
+		if(creep.memory.role == 'worker') {
             	roleWorker.run(creep);
         	}
 		else if (creep.memory.role == 'guard') {
@@ -65,23 +43,9 @@ module.exports.loop = function () {
 		}
     	}
 	
-	//let response = extensionConstructor.run(Game.spawns['Spawn1']);
-
-	//if (response !== OK) {
-	//	spawnStorageConstructor.run(Game.spawns['Spawn1']); 
-	//	spawnDefenseConstructor.run(Game.spawns['Spawn1']);
-	//	spawnConstructor.run(Game.spawns['Spawn1']);
-	//}
-
-	//let hasRoads = Memory.hasRoads;
-	//let constructionSites = Game.spawns['Spawn1'].room.find(FIND_MY_CONSTRUCTION_SITES);
-	//if (!hasRoads && Game.cpu.bucket === 10000 && constructionSites.length === 0) {
-	//	roadConstructor.run(Game.spawns['Spawn1'].room);
-	//}
-    
-    if(Memory.expansion === false && Game.cpu.bucket == 10000) {
-        Game.cpu.generatePixel();
-    }
+   	if(Memory.expansion === false && Game.cpu.bucket == 10000) {
+       	Game.cpu.generatePixel();
+    	}
 	else if (Memory.expansion === true) {
 		expansionController.run();
 	}
@@ -124,6 +88,3 @@ module.exports.loop = function () {
 		}
 	}
 };
-
-
-//STRUCTURE_SPAWN constant for spawn building
