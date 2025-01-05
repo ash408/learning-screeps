0ce7d46e10898ce67e52220ea1963f2c385edca7
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-04 23:03:27 -0500

    Move globals to separate module

--- test/main.js
+++ test/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require('global.js');
+
 let roadConstructor = require('road.constructor');
 let colonyController = require('colony.controller');
 let expansionController = require('expansion.controller');
@@ -29,43 +31,5 @@ module.exports.loop = function () {
     	}
 	else if (Memory.expansion === true) {
 		expansionController.run();
-	}
-
-	global.clearSites = function() {
-		let spawnHash = Game.spawns;
-		let spawns = Object.keys(spawnHash).map(function(v) { return spawnHash[v]; });
-		let sites = [];
-
-		for (let spawn of spawns){
-			let thisSites = spawn.room.find(FIND_MY_CONSTRUCTION_SITES);
-			sites = [...sites, ...thisSites];
-		}
-		for (let site of sites) {
-			site.remove();
-		}
-	}
-
-	global.startExpansion = function(startRoom, expansionTarget) {
-		Memory.expansion = true;
-		Memory.startExpansionRoom = startRoom;
-		Memory.expansionTarget = expansionTarget;
-	}
-
-	global.clearWorkers = function() {
-		for (let name in Game.creeps) {
-			let creep = Game.creeps[name];
-
-			if(creep.memory.role == 'worker') {
-				creep.memory.task = 'harvesting';
-			}
-		}
-	}
-
-	global.createRoads = function(roomName) {
-		let room = Game.rooms[roomName];
-
-		if (room !== undefined) {
-			roadConstructor.run(room);
-		}
 	}
 };
