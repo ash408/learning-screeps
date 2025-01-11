e0e55db16d11e045dcc351da73d58942ded3fd0a
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-11 07:52:18 -0500

    Towers now heal

--- test/role.tower.js
+++ test/role.tower.js
@@ -5,8 +5,17 @@ let roleTower = {
 
 	run: function(tower) {
 		let target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+		let injured = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+			filter:(c) => {
+				return c.hits < c.hitsMax;
+			}
+		});
+
 		if (target !== null) {
 			tower.attack(target);
+		}
+		else if (injured !== null {
+			tower.heal(inured);
 		}
 	}
 };
