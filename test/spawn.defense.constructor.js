0091d86927eaabf32b087759f3e13d2f3605ab34
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-11 08:40:27 -0500

    Add second tower at rcl 5

--- test/spawn.defense.constructor.js
+++ test/spawn.defense.constructor.js
@@ -7,10 +7,23 @@ let spawnDefenseConstructor = {
 		let x = spawn.pos.x + 1;
 		let y = spawn.pos.y + 1;
 
-		let hasTower = spawn.room.lookForAt(LOOK_STRUCTURES, x, y).length !== 0;
-		if (!hasTower) {
+		let x2 = spawn.pos.x + 1;
+		let y2 = spawn.pos.y - 1;
+
+		if (!this.hasTower(x, y)) {
 			spawn.room.createConstructionSite(x, y, STRUCTURE_TOWER);
 		}
+		else {
+			let rcl = spawn.room.controller.level;
+
+			if (rcl > 4 && !this.hasTower(x2, y2))) {
+				spawn.room.createConstructionSite(x2, y2, STRUCTURE_TOWER);
+			}
+		}
+	},
+
+	hasTower: function(x, y) {
+		return spawn.room.lookForAt(LOOK_STRUCTURES, x, y).length !== 0;
 	}
 };
 
