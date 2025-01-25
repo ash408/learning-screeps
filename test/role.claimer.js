74a1dddd79b49efea6f3f048795c41994a62cca0
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-25 07:54:45 -0500

    Add max room for claimer path

--- test/role.claimer.js
+++ test/role.claimer.js
@@ -6,13 +6,13 @@ let roleClaimer = {
 	run: function(creep) {
 		if (creep.room.name !== Memory.expansionTarget) {
 			let exits = creep.room.find(creep.room.findExitTo(Memory.expansionTarget));
-			creep.moveTo(exits[0], {visualizePathStyle: {stroke: '#ffffff'}});
+			creep.moveTo(exits[0], {visualizePathStyle: {stroke: '#ffffff'}, maxRooms: 1});
 		}
 		else {
 			let response = creep.claimController(creep.room.controller);
 
 			if(response === ERR_NOT_IN_RANGE) {
-				creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+				creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}, maxRooms: 1});
 			}
 			else {
 				console.log('Room ' + Memory.expansionTarget + ' claimed');
