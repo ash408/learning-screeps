5dddaa53074211b95a5406e11f458c46fe5bcae0
ash408 <124532093+ash408@users.noreply.github.com>
2025-01-25 07:52:16 -0500

    Fix guard filter

--- test/spawn.controller.js
+++ test/spawn.controller.js
@@ -122,7 +122,8 @@ let spawnController = {
 	spawnCleaner: function(spawn, target) {
 		if (!spawn.spawning) {
 			let creeps = Game.creeps;
-			let guards = _.filter(creeps, (creep) => creep.memory.room === target.name && creep.memory.role === 'guard');
+			let guards = _.filter(creeps, {filter: (creep) => {
+				return (creep.memory.room !== undefined && creep.memory.room === target.name && creep.memory.role === 'guard')}};
 
 			if (guards.length < MAX_CLEANRES) {
 				let newName = 'Cleaner' + Game.time;
