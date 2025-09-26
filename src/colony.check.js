a2edc38f3217199606978642d753f6124a78d43e
ash408 <124532093+ash408@users.noreply.github.com>
2025-09-26 09:49:11 -0500

    Add untracked files from code rotation

---
+++ src/colony.check.js
@@ -0,0 +1,110 @@
+"use strict";
+
+let roadConstructor = require('road.constructor');
+let spawnConstructor = require('spawn.constructor');
+let spawnStorageConstructor = require('spawn.storage.constructor');
+let spawnDefenseConstructor = require('spawn.defense.constructor');
+
+
+let colonyCheck = {
+
+	run: function() {
+		let roomHash = Game.rooms
+		let allRooms = Object.keys(roomHash).map(function(v) { return roomHash[v];});
+		let claimed = this.getClaimed(allRooms);
+
+		this.rebuildCheck(claimed);
+		if (Memory.expansion === false) {
+			this.expansionCheck(claimed);
+		}
+	},
+
+	expansionCheck: function(claimed) {
+		for(let room of claimed) {
+
+			let roomController = room.controller;
+			let numConstruction = room.find(FIND_MY_CONSTRUCTION_SITES).length
+
+			if(roomController.level >= 4 && claimed.length < 5 && numConstruction === 0) {
+				let adjacentRooms = this.getAdjacentRooms(room);
+
+				for (let adjacentRoom of adjacentRooms) {
+
+					if (this.validateClaimable(adjacentRoom)) {
+						console.log("Claiming room " + adjacentRoom.name + " from " + room.name);
+						global.startExpansion(room.name, adjacentRoom.name);
+					}		
+				}
+			}
+		}
+	},
+
+	rebuildCheck: function(claimed) {
+		for (let room of claimed) {
+			let spawns = room.find(FIND_MY_SPAWNS);
+			let numSpawns = spawns.length;
+			let numConstruction = room.find(FIND_MY_CONSTRUCTION_SITES).length;
+			let rcl = room.controller.level;
+
+			if (numSpawns !== 0) {
+				spawnStorageConstructor.run(spawns[0]);
+			}
+
+			if (numSpawns === 0 && numConstruction === 0) {
+				spawnConstructor.buildSpawn(room);
+			}
+			else if (numSpawns !== 0 && rcl >= 3 && numConstruction === 0) {
+				spawnDefenseConstructor.run(spawns[0]);
+				roadConstructor.run(room);
+			}
+		}
+	},
+
+	getClaimed: function(allRooms) {
+		let claimed = [];
+		
+		for (let room of allRooms) {
+			let roomController = room.controller;
+
+			if (roomController!== undefined && roomController.my) {
+				claimed.push(room);
+			}
+		}
+
+		console.log("Num of rooms claimed: " + claimed.length.toString());
+		return claimed;
+	},
+
+	validateClaimable: function(room) {
+		let isClaimable = false;
+
+		let roomController = room.controller;
+		console.log("Room controller owner is: " + room.controller.owner);
+		if(roomController !== undefined && !roomController.my &&
+			roomController.reservation === undefined &&
+			roomController.owner === undefined) {
+
+			console.log("Room " + room.name + " is claimable!");
+			isClaimable = true;
+			
+		}
+		return isClaimable;
+	},
+
+	getAdjacentRooms: function(room) {
+		let adjacentRoomHash = Game.map.describeExits(room.name);
+		let adjacentRoomNames = Object.keys(adjacentRoomHash).map(function(v) { return adjacentRoomHash[v];});
+		let adjacentRooms = [];
+		console.log("Adjacent names: " + adjacentRoomNames);		
+
+		for (let room of adjacentRoomNames) {
+			let allRooms = Game.rooms;
+			console.log("Checking room: " + room);
+			if (room in allRooms) { adjacentRooms.push(allRooms[room]); }
+		}
+		console.log("Visible rooms: " + adjacentRooms)
+		return adjacentRooms;
+	}
+};
+
+module.exports = colonyCheck;
---
+++ src/role.scout.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+let roleScout = {
+
+	run: function(creep) {
+		let targetRoom = creep.memory.room;
+
+		if (targetRoom === undefined) {
+			let adjacentRoomHash = Game.map.describeExits(creep.room.name);
+			let adjacentRoomNames = Object.keys(adjacentRoomHash).map(function(v) {return adjacentRoomHash[v];});
+
+			let randomIndex = Math.floor(Math.random() * adjacentRoomNames.length);
+			creep.memory.room = adjacentRoomNames[randomIndex];	
+		}
+
+		if (creep.room.name !== targetRoom) {
+			let exits = creep.room.find(creep.room.findExitTo(targetRoom));
+			let nearestExit = creep.pos.findClosestByPath(exits);
+
+			creep.moveTo(nearestExit, {visualizePathStyle: {stroke: "#ffffff"}, maxRooms: 1});
+		}
+	}
+};
+
+module.exports = roleScout;
